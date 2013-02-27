d6b50f9b5118ed235e8866d56f4bc4a5e5657ae2
Samvit Ramadurgam <rsamvit@gmail.com>
2013-02-26 21:21:26 -0800

    middle of scraping

--- src/scraper/main.clj
+++ src/scraper/main.clj
@@ -6,12 +6,59 @@
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn hn-headlines []
-  (map html/text (html/select (fetch-url *base-url*) [:td.title :a])))
+(defn hn-headlines-and-points []
+  (map html/text
+       (html/select (fetch-url *base-url*)
+                    #{ 
+                      [:td.title (:not(:a [:rel "nofollow"]))] 
+                      [:td.subtext (html/nth-child 3)]
+                      ;;[:td.subtext :a html/first-child]
+                      })))
 
+(defn selector [hiccup]
+  (map html/text
+       (html/select (fetch-url *base-url*)
+                    #{ 
+                      hiccup
+                      }
+                    ))
+  )
+
+(defn hn-comments []
+  (selector [:td.title (:not(:a [:rel "nofollow"]))]))
+
+(defn hn-comments []
+  (map html/text
+       (html/select (fetch-url *base-url*)
+                    #{ 
+                      [:td.subtext (html/nth-child 3)]
+                      }
+                    )))
 (defn hn-points []
-  (map html/text (html/select (fetch-url *base-url*) [:td.subtext html/first-child])))
+  (map html/text
+       (html/select (fetch-url *base-url*)
+                    #{ 
+                      [:td.subtext (html/first-child 3)]
+                      }
+                    )))
 
-(defn print-headlines-and-points []
-  (doseq [line (map #(str %1 " (" %2 ")") (hn-headlines) (hn-points))]
+(defn hn-headlines []
+  (map html/text
+       (html/select (fetch-url *base-url*)
+                    #{ 
+                      [:td.title (:not(:a [:rel "nofollow"]))] 
+                      }
+                    )))               
+
+(defn print-headlines-and-points [sel-func]
+  (doseq [line (map (fn [[title]] 
+                      (str title)
+                      )
+                    (partition 1 (sel-func)))]
     (println line)))
+
+(defn run-all []
+  (print-headlines-and-points hn-headlines)
+  (print-headlines-and-points hn-comments)
+  (print-headlines-and-points hn-points)
+  )
